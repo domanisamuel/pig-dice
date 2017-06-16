e86db204eec5d93e4e7089e360a6173ddd1265c5
domanisamuel <wafulasammie@gmail.com>
2017-06-16 16:34:44 +0300

    finished working on business logic and started user interface logic

--- JS/script.js
+++ JS/script.js
@@ -13,3 +13,57 @@ function Player(turn){
 	this.turn = turn;
 	this.playerName;
 }
+
+Player.prototype.rollone = function(){
+	if(this.roll === 1){
+		this.tempscore = 0;
+		alert("Sorry " + this.playerName + ", you rolled a 1! \n Your turn is over!")
+	}
+	else{
+		this.tempscore += this.roll;
+	}
+}
+
+Player.prototype.hold = function(){
+	this.totalscore += this.tempscore;
+	this.tempscore = 0;
+	alert(this.playerName + ", your turn is over!");
+}
+
+Player.prototype.winnerCheck = function(){
+	if(this.totalscore >= 100){
+		alert(this.playerName + ", You are the winner!");
+	}
+}
+
+Player.prototype.newGame = function(){
+	this.roll = 0;
+	this.tempscore = 0;
+	this.totalscore = 0;
+	this.playerName = 0;
+}
+
+//User Interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
